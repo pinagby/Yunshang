d58afe5d6a0bd62aadd733fb6edd90aa28b2ef88
Haulyn Jason <dc@yunshang.org>
2011-08-11 17:31:22 +0800

    finished basic configuration for customerbundle, managebundle and marketbundle with a very very simple login form, but I have a bad news, the firewall for authentication and authorizaion does not work, I can access any urls which should be protected by the firewall without any login action. confused. Before, I am familiar with Symfony 2 RC and Beta version, now we upgrade to the stable version of Symfony2, maybe something tips changed, I need to debug it more. Thanks guys, at least we have some pages now.

--- application/src/Yunshang/Bundle/ManageBundle/Controller/DefaultController.php
+++ application/src/Yunshang/Bundle/ManageBundle/Controller/DefaultController.php
@@ -9,11 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/",name="manage_index")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array('name' => 'fuck gfw');
     }
 }
