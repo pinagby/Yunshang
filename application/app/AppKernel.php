20cbc3241979d02be907ea8cb4bce3049590b6e1
Haulyn Jason <dc@yunshang.org>
2011-08-11 17:24:53 +0800

    add YunshangCustomerBundle and make it under ^/customer/, secured by firewall under customer_section. Now we have 4 bundles, YunshangMarketBundle as market, YunshangCustomerBundle as customers' section, admin use YunshangManageBundle, all these three bundles refer to YunshangCommonBundle.

--- application/app/AppKernel.php
+++ application/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Yunshang\Bundle\CommonBundle\YunshangCommonBundle(),
             new Yunshang\Bundle\ManageBundle\YunshangManageBundle(),
             new Yunshang\Bundle\MarketBundle\YunshangMarketBundle(),
+            new Yunshang\Bundle\CustomerBundle\YunshangCustomerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ application/src/Yunshang/Bundle/CustomerBundle/YunshangCustomerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Yunshang\Bundle\CustomerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YunshangCustomerBundle extends Bundle
+{
+}
