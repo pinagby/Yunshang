f42a51ce30dce5ab393be51df56e1876e7ff50db
Haulyn Jason <dc@yunshang.org>
2011-08-26 01:40:25 +0800

    add drop down menu prototype

--- application/src/Yunshang/Bundle/ManageBundle/Controller/System/ConfigurationController.php
+++ application/src/Yunshang/Bundle/ManageBundle/Controller/System/ConfigurationController.php
@@ -4,7 +4,6 @@ namespace Yunshang\Bundle\ManageBundle\Controller\System;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
