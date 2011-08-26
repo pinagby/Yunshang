3d161cc056dd6e0a22797c9705c60513a3543e85
Haulyn Jason <dc@yunshang.org>
2011-08-27 00:56:57 +0800

    Create Tag as Infrastructure for whole yunshang project, remember to execute  app/console doctrine:schema:update --force after you pull and before visiting the homepage

---
+++ application/src/Yunshang/Bundle/CommonBundle/Entity/Infrastructure/Tag.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Yunshang\Bundle\CommonBundle\Entity\Infrastructure;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Yunshang\Bundle\CommonBundle\Entity\Infrastructure\Tag
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Tag
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $type
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var datetime $created
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var datetime $modified
+     *
+     * @ORM\Column(name="modified", type="datetime")
+     */
+    private $modified;
+
+    /**
+     * @var boolean $status
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set created
+     *
+     * @param datetime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * Get created
+     *
+     * @return datetime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param datetime $modified
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return datetime 
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
