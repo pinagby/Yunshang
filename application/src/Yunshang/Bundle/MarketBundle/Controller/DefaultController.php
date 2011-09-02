ce89865bc68fa6d2633f232bd468ffca8d6b0e70
Haulyn Jason <dc@yunshang.org>
2011-09-02 14:52:14 +0800

    add query on index page

--- application/src/Yunshang/Bundle/MarketBundle/Controller/DefaultController.php
+++ application/src/Yunshang/Bundle/MarketBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace Yunshang\Bundle\MarketBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Yunshang\Bundle\MarketBundle\Entity\Product;
+use Yunshang\Bundle\MarketBundle\Form\ProductType;
 
 class DefaultController extends Controller
 {
@@ -14,8 +17,38 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        /*@todo remove */
         $yunshangService = $this->get('YunshangCommonBundle.yunshangService');
         $yunshangService->init();
-        return array();
+
+        
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('YunshangMarketBundle:Product')->findAll();
+
+        return array('entities' => $entities);
     }
+    /**
+     * Finds and displays a Product entity.
+     *
+     * @Route("/{id}/show", name="market_product_show")
+     * @Template()
+     */
+    public function showProductAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('YunshangMarketBundle:Product')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Product entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        );
+    }
+
 }
