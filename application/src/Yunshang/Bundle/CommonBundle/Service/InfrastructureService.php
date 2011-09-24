10a8cb844c9a4b7f0dd439fc13111436c396cd79
Haulyn Jason <dc@yunshang.org>
2011-09-24 22:41:31 +0800

    add function getBasePath

--- application/src/Yunshang/Bundle/CommonBundle/Service/InfrastructureService.php
+++ application/src/Yunshang/Bundle/CommonBundle/Service/InfrastructureService.php
@@ -1,9 +1,23 @@
 <?php
 namespace Yunshang\Bundle\CommonBundle\Service;
 
+
+use Symfony\Component\HttpFoundation\Request;
+
+
 class InfrastructureService
 {
+    private Request $request ;
+
+    public function __construct(Request $request){
+        $this->request = $request ;
+    }
+    
     public function getCurrentDatetime(){
         return date_create(date("F j, Y, g:i a"));
+    }
+
+    public function getBasePath(){
+        return $this->request->getBasePath();
     }
 }
